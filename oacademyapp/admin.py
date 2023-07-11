acd6ee3a2c865a24ddee63d753c2f41515fa7bd5
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-11 18:27:43 +0100

    Update admin.py

--- oacademyapp/admin.py
+++ oacademyapp/admin.py
@@ -4,4 +4,4 @@ from . import models
 # Register your models here.
 admin.site.register(models.Notes)
 admin.site.register(models.Assignments)
-admin.site.register(models.Tasks)
+admin.site.register(models.Todo)
