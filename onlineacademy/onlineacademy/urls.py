b6a7b0824e93165f9b4d2cb3cd0f33c23de53f9f
Chineze Ogugua <christabelogugua@gmail.com>
2023-07-12 08:18:12 -0700

    starting project

--- onlineacademy/onlineacademy/urls.py
+++ onlineacademy/onlineacademy/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for oacademyapp project.
+URL configuration for onlineacademy project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,15 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from oacademyapp.views import register, profile
-from django.contrib.auth import views as auth_views
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('oacademyapp.urls')),
-    path('register/', register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('profile/', profile, name='profile'),
 ]
--- onlineacademy/onlineacademy/wsgi.py
+++ onlineacademy/onlineacademy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for oacademyapp project.
+WSGI config for onlineacademy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
