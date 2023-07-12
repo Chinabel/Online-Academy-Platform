b77d50fe29c86aec3b5fa5c00a723d09b67df7f9
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-12 12:47:57 +0100

    Update settings.py

--- Onlineacademy/settings.py
+++ Onlineacademy/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-4rm9j^6^=0n#^3ub&3wta2l#pl&$^i6g&pfxhl*dw1_un=#)e^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
