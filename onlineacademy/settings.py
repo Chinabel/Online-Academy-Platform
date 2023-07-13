1b16ec5e247aea5016426e698d412138f1dd7198
Chineze Ogugua <christabelogugua@gmail.com>
2023-07-13 14:37:41 -0700

    fix settings

--- onlineacademy/settings.py
+++ onlineacademy/settings.py
@@ -26,7 +26,7 @@ env=environ.Env()
 environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = "r@UH$%^&123456"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
