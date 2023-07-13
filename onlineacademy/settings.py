d56bef5aa9eda026646966b61febe95731479b29
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-13 09:22:37 +0100

    Update settings.py

--- onlineacademy/settings.py
+++ onlineacademy/settings.py
@@ -131,14 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 #for deployment
 STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
 #for development
 STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, 'static/')
+	os.path.join(BASE_DIR, 'static')
 
 #for compression support
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
