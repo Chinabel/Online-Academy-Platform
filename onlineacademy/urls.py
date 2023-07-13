f64bcc3ce5b55751de097025e6738fd9305b1f30
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-13 22:27:16 +0100

    Update urls.py

--- onlineacademy/urls.py
+++ onlineacademy/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from oacademyapp.views import register, profile
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('oacademyapp.urls')),
+    path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('profile/', profile, name='profile'),
 ]
