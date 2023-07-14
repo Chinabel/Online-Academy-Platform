7cb3c2984be1be668747d3392b0b794fd0b29893
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-14 09:03:47 +0100

    Update urls.py

--- onlineacademy/urls.py
+++ onlineacademy/urls.py
@@ -21,10 +21,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    path(r'^admin/', include('admin.site.urls')),
+    path(r'^admin', include('admin.site.urls')),
     path('', include('oacademyapp.urls')),
-    path('register/', register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('profile/', profile, name='profile'),
+    path('register', register, name='register'),
+    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('profile', profile, name='profile'),
 ]
