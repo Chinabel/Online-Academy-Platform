41efd25d233015f0baab0a7332c841441c5cec05
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-14 12:29:26 +0100

    Update settings.py

--- onlineacademy/settings.py
+++ onlineacademy/settings.py
@@ -89,9 +89,6 @@ WSGI_APPLICATION = 'onlineacademy.wsgi.application'
 
 DATABASES = {
     'default': {
-         #'ENGINE': 'django.db.backends.sqlite3',
-         #'NAME': BASE_DIR / 'db.sqlite3',
-
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
          'NAME': os.getenv('onlineacademy'),
          'USER': os.getenv('***'),
