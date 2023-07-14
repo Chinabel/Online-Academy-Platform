80cf12ff5569c68b1551468751ce803ad651f55e
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-14 10:00:09 +0100

    Update apps.py

--- oacademyapp/apps.py
+++ oacademyapp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class OacademyappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'oacademyapp'
+    verbose_name = 'A Much Better Name'
