e29e6859c8a93fd4e5411069e9e47174048977cb
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-14 09:12:47 +0100

    Update views.py

--- oacademyapp/views.py
+++ oacademyapp/views.py
@@ -3,33 +3,35 @@ from django.shortcuts import render, redirect
 from .forms import *
 from django.contrib import messages
 from django.views import generic
+from django.urls import reverse
 from youtubesearchpython import VideosSearch
 import requests
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'dashboard/home.html')
 
 @login_required
 def notes(request):
    if request.method=="POST":
       form=NotesForm(request.POST)
       if form.is_valid():
-         notes=Notes(user=request.user, title=request.POST['title'], description=request.POST['description'])
-         notes.save()
-         messages.success(request, "Notes Added Successfully")
-         return redirect('notes')
+          title = form.cleaned_data['title']
+          descrip = form.cleaned_data['description']
+          notes=Notes(user=request.user, title=request.POST['title'], description=request.POST['description'])
+          notes.save()
+          messages.success(request, "Notes Added Successfully")
+          return redirect('notes')
 
    else:
       form=NotesForm()
-
    notes=Notes.objects.filter(user=request.user)
    context={
       'notes':notes,
       'form':form,
    }
-   return render(request, 'notes.html',context)
+   return render(request, 'dashboard/notes.html',context)
 
 @login_required
 def delete_note(request,pk=None):
