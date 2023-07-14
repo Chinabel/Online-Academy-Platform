3b8bf6148041ff01859d12e9742ee0893b613538
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-14 10:24:21 +0100

    Update urls.py

--- onlineacademy/urls.py
+++ onlineacademy/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from oacademy import views
 from oacademyapp.views import register, profile
+from .views import index
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
