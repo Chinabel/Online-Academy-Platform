e1f7a22df2da2d8f0347bf855584da19e76274e7
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-14 09:00:11 +0100

    Update models.py

--- oacademyapp/models.py
+++ oacademyapp/models.py
@@ -7,12 +7,11 @@ class Notes(models.Model):
     title=models.CharField(max_length=200)
     description=models.TextField()
 
+    def __str__(self):
+        return self.title
     class Meta:
         verbose_name="notes"
         verbose_name_plural="notes"
-
-    def __str__(self):
-        return self.title
 
 
 class Assignments(models.Model):
