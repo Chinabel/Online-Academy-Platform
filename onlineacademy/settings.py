7b2f8712b6375182a5049672553fc71c4bf5bbb2
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-14 16:42:39 +0100

    Update settings.py

--- onlineacademy/settings.py
+++ onlineacademy/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
          'USER': os.getenv('***'),
          'PASSWORD': os.getenv('***'),
          'HOST':os.getenv('*'),
-         'PORT': '5432',
+         'PORT': '22',
     }
 }
 
