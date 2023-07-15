999a2bb56f7774cbb44f5ca1860c22d202cbe03f
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-15 21:51:38 +0100

    Update urls.py

--- onlineacademy/urls.py
+++ onlineacademy/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('profile/', profile, name='profile'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
