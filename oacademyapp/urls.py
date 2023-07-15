2f007bd5718f400ad732599860b081e9040b84af
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-15 22:34:44 +0100

    Update urls.py

--- oacademyapp/urls.py
+++ oacademyapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path(r'^$', views.index, name='index'),
     path('',views.home, name='home'),
 
     path('notes/',views.notes, name="notes"),
