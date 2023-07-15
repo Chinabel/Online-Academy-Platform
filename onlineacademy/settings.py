48dd1de245858f488bafa92824f3be25220805cb
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-15 21:35:04 +0100

    Update settings.py

--- onlineacademy/settings.py
+++ onlineacademy/settings.py
@@ -141,7 +141,7 @@ STATIC_ROOT= os.path.join(BASE_DIR,'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # location where you will store your static files
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'home/static'), )
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
