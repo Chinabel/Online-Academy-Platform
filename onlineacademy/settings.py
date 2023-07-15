7f30c9262e31e5e700b45cec5ba137e0129d30e1
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-15 22:02:13 +0100

    Update settings.py

--- onlineacademy/settings.py
+++ onlineacademy/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 #location where django collect all static files
 STATIC_ROOT= "/var/www/oacademy-ffa5f69c1d76.herokuapp.com/static/"
