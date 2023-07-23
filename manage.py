19ddf6c4b0f3cc34c9d1ea31d46481f7fcbc32d4
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-07-23 19:28:56 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,19 +4,22 @@ import os
 import sys
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlineacademy.settings')
+if __name__ == "__main__":
+    if not os.environ.get('DJANGO_ENV', False):
+        raise EnvironmentError('Please define DJANGO_ENV environment variable')
+    
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'config.settings.{}'.format(os.environ.get('DJANGO_ENV')))
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    except ImportError:
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
