704ee66c41d7e072c97468b2c45bf05f093f1627
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2024-12-13 14:40:08 +0000

    Update project folder modules

--- onlineacademy/urls.py
+++ onlineacademy/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from codecs import register
+import profile
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from oacademyapp.views import register, profile
+
 from django.contrib.auth import views as auth_views
 from oacademyapp import views
 
