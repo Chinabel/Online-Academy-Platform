b4092b503c4d2584f632b74443c9ee1947a41063
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2025-01-09 21:43:31 +0000

    update on base.html, language.html and urls

--- oacademyapp/urls.py
+++ oacademyapp/urls.py
@@ -1,9 +1,8 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from .views import logout_view, login_view
-from django.views.i18n import set_language
 from . import views
 
 app_name = 'oacademyapp'
@@ -16,13 +15,12 @@ urlpatterns = [
     path('todo/', views.todo, name='todo'),
     path('profile/', views.profile, name='profile'),
     path('youtube/', views.youtube_video_list, name='youtube'),
-    path('login/', login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('logout/', logout_view, name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
     path('books/', views.books, name='books'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('accounts/', include('allauth.urls')),
-    path('set-language/', include('django.conf.urls.i18n')),
 ]
